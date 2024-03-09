fbd29264eb42bbbefd8fd9bfd06e839701ac1eae
i11usion <igoryunnn@yandex.ru>
2024-03-09 12:51:36 +0300

    исправление ошибок

--- src/main/java/ru/practicum/shareit/item/comment/Comment.java
+++ src/main/java/ru/practicum/shareit/item/comment/Comment.java
@@ -17,16 +17,20 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "text")
     private String text;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @ToString.Exclude
     @JoinColumn(name = "item_id")
     private Item item;
+
     @OneToOne
     @ToString.Exclude
     @JoinColumn(name = "author_id")
     private User author;
+
     @Column(name = "created")
     LocalDateTime created;
 }
